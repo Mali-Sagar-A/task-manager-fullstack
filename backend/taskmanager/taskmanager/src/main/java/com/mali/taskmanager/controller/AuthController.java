eba858f0c1f7b975222faf59d7a9744b49158c37
Mali-Sagar-A <malisagar2097@gmail.com>
2026-02-10 23:53:38 +0530

    Configure Spring Security to allow auth endpoints

---
+++ backend/taskmanager/taskmanager/src/main/java/com/mali/taskmanager/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.mali.taskmanager.controller;
+
+import com.mali.taskmanager.model.User;
+import com.mali.taskmanager.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "http://localhost:3000")
+public class AuthController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/register")
+    public String register(@RequestBody User user) {
+
+        if (userRepository.existsByEmail(user.getEmail())) {
+            return "Email already registered";
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+
+        return "User registered successfully";
+    }
+}
