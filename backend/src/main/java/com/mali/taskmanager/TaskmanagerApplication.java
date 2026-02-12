bca1214cddf0acf8e1c8a270f5110614f2afd48d
Mali-Sagar-A <malisagar2097@gmail.com>
2026-02-12 15:56:09 +0530

    Fix Spring Security config loading and package scan

--- backend/src/main/java/com/mali/taskmanager/TaskmanagerApplication.java
+++ backend/src/main/java/com/mali/taskmanager/TaskmanagerApplication.java
@@ -2,8 +2,10 @@ package com.mali.taskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.mali.taskmanager")
 public class TaskmanagerApplication {
 
 	public static void main(String[] args) {
